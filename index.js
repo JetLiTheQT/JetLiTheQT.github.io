f8dfe3a01404df7efd4df22224815ef1522a2907
Jet Ittihrit <ekkachai.ittihrit@gmail.com>
2022-12-01 17:01:10 -0800

    fixed js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import initScrollReveal from "./scrollReveal.js";
 import initTiltEffect from "./tiltAnimation.js";
-import { targetElements, defaultProps } from "scrollRevealConfig.js";
+import { targetElements, defaultProps } from "./scrollRevealConfig.js";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
